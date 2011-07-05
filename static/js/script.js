ed2bb96bdd191d9740e5c872293febf515426166
Rob Eroh <rob@eroh.me>
2011-07-04 18:15:40 -0700

    update example

--- static/js/script.js
+++ static/js/script.js
@@ -2,20 +2,18 @@
 
 */
 
-$(document).ready(function() {   
-   
-   io.setPath('/client/');
-   socket = new io.Socket(null, { 
-     port: 8081
-     ,transports: ['websocket', 'htmlfile', 'xhr-multipart', 'xhr-polling']
+$(document).ready(function() { 
+   var chat = io.connect('http://localhost');
+
+   chat.on('connect', function () {
+     chat.emit('hi!');
    });
-   socket.connect();
     
    $('#sender').bind('click', function() {
-     socket.send("Message Sent on " + new Date());     
+     chat.send("Message Sent on " + new Date());     
    });
    
-   socket.on('message', function(data){
+   chat.on('message', function(data){
      $('#reciever').append('<li>' + data + '</li>');  
    });
       
