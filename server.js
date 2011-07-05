c3bf42cf992e002ce5393e8e4dc11dd3113fd885
Rob Eroh <rob@eroh.me>
2011-07-04 17:23:40 -0700

    fixed some formatting that bothers me

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require(__dirname + "/lib/setup").ext( __dirname + "/lib").ext( __dirname + "/li
 var connect = require('connect')
     , express = require('express')
     , sys = require('sys')
-    , io = require('Socket.IO-node')
+    , io = require('socket.io')
     , port = (process.env.PORT || 8081);
 
 //Setup Express
@@ -19,38 +19,38 @@ server.configure(function(){
 server.error(function(err, req, res, next){
     if (err instanceof NotFound) {
         res.render('404.ejs', { locals: { 
-                  header: '#Header#'
-                 ,footer: '#Footer#'
-                 ,title : '404 - Not Found'
-                 ,description: ''
-                 ,author: ''
-                 ,analyticssiteid: 'XXXXXXX' 
-                },status: 404 });
+            header: '#Header#',
+           footer: '#Footer#',
+           title : '404 - Not Found',
+           description: '',
+           author: '',
+           analyticssiteid: 'XXXXXXX' 
+        }, status: 404 });
     } else {
         res.render('500.ejs', { locals: { 
-                  header: '#Header#'
-                 ,footer: '#Footer#'
-                 ,title : 'The Server Encountered an Error'
-                 ,description: ''
-                 ,author: ''
-                 ,analyticssiteid: 'XXXXXXX'
-                 ,error: err 
-                },status: 500 });
+           header: '#Header#',
+           footer: '#Footer#',
+           title : 'The Server Encountered an Error',
+           description: '',
+           author: '',
+           analyticssiteid: 'XXXXXXX',
+           error: err 
+        }, status: 500 });
     }
 });
-server.listen( port);
+server.listen(port);
 
 //Setup Socket.IO
 var io = io.listen(server);
 io.on('connection', function(client){
-	console.log('Client Connected');
-	client.on('message', function(message){
-		client.broadcast(message);
-		client.send(message);
-	});
-	client.on('disconnect', function(){
-		console.log('Client Disconnected.');
-	});
+    console.log('Client Connected');
+    client.on('message', function(message){
+    client.broadcast(message);
+        client.send(message);
+    });
+    client.on('disconnect', function(){
+        console.log('Client Disconnected.');
+    });
 });
 
 
@@ -60,35 +60,35 @@ io.on('connection', function(client){
 
 /////// ADD ALL YOUR ROUTES HERE  /////////
 
-server.get('/', function(req,res){
+server.get('/', function(req, res){
   res.render('index.ejs', {
     locals : { 
-              header: '#Header#'
-             ,footer: '#Footer#'
-             ,title : 'Page Title'
-             ,description: 'Page Description'
-             ,author: 'Your Name'
-             ,analyticssiteid: 'XXXXXXX' 
-            }
+        header: '#Header#'
+        footer: '#Footer#'
+        title : 'Page Title'
+        description: 'Page Description'
+        author: 'Your Name'
+        analyticssiteid: 'XXXXXXX' 
+    }
   });
 });
 
 
 //A Route for Creating a 500 Error (Useful to keep around)
-server.get('/500', function(req, res){
+server.get('/500', function(req, res) {
     throw new Error('This is a 500 Error');
 });
 
 //The 404 Route (ALWAYS Keep this as the last route)
-server.get('/*', function(req, res){
+server.get('/*', function(req, res) {
     throw new NotFound;
 });
 
-function NotFound(msg){
+function NotFound(msg) {
     this.name = 'NotFound';
     Error.call(this, msg);
     Error.captureStackTrace(this, arguments.callee);
 }
 
 
-console.log('Listening on http://0.0.0.0:' + port );
+console.log('Listening on http://0.0.0.0:' + port);
